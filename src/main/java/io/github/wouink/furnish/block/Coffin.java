98920ed4e419e31a9a58f04aab0be7e626a0093d
Wouink <winkontact@gmail.com>
2022-10-23 11:48:24 +0200

    Halloween Update - Shift-RClick a Coffin to open/close.

--- src/main/java/io/github/wouink/furnish/block/Coffin.java
+++ src/main/java/io/github/wouink/furnish/block/Coffin.java
@@ -1,7 +1,12 @@
 package io.github.wouink.furnish.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -9,6 +14,8 @@ import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BedPart;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -21,6 +28,28 @@ public class Coffin extends BedBlock {
 	@Override
 	public void fallOn(Level p_152169_, BlockState p_152170_, BlockPos p_152171_, Entity p_152172_, float p_152173_) {
 		Blocks.OAK_PLANKS.fallOn(p_152169_, p_152170_, p_152171_, p_152172_, p_152173_);
+	}
+
+	@Override
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+		if(player.isCrouching()) {
+			if(level.isClientSide()) return InteractionResult.CONSUME;
+			boolean occupied = state.getValue(OCCUPIED);
+			setOccupied(level, pos, !occupied);
+			if(occupied) level.playSound(null, pos, SoundEvents.CHEST_OPEN, SoundSource.BLOCKS, 1.0f, 1.0f);
+			else level.playSound(null, pos, SoundEvents.CHEST_CLOSE, SoundSource.BLOCKS, 1.0f, 1.0f);
+			return InteractionResult.SUCCESS;
+		} else return super.use(state, level, pos, player, hand, hitResult);
+	}
+
+	public void setOccupied(Level level, BlockPos pos, boolean occupied) {
+		BlockState state = level.getBlockState(pos);
+		if(state.is(this)) {
+			BlockPos otherPart = pos.relative(state.getValue(PART) == BedPart.FOOT ? state.getValue(FACING) : state.getValue(FACING).getOpposite());
+			BlockState otherState = level.getBlockState(otherPart);
+			level.setBlockAndUpdate(pos, state.setValue(OCCUPIED, occupied));
+			level.setBlockAndUpdate(otherPart, otherState.setValue(OCCUPIED, occupied));
+		}
 	}
 
 	@Override
